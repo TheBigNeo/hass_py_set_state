b42fc500ddf88339f5194cc33091d6de872b5a26
TheBigNeo <alexandro.will@outlook.com>
2023-07-20 23:18:07 +0200

    Add state_id

--- python_scripts/set_state.py
+++ python_scripts/set_state.py
@@ -1,16 +1,18 @@
-#==================================================================================================
-#  python_scripts/set_state.py 
+# ==================================================================================================
+#  python_scripts/set_state.py
 #  modified from - https://community.home-assistant.io/t/how-to-manually-set-state-value-of-sensor/43975/37
-#==================================================================================================
+# ==================================================================================================
 
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 # Set the state or other attributes for the entity specified in the Automation Action
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+
+logger.info("Set State")
 
 inputEntity = data.get('entity_id')
 if inputEntity is None:
     logger.warning("===== entity_id is required if you want to set something.")
-else:    
+else:
     inputStateObject = hass.states.get(inputEntity)
     if inputStateObject is None and not data.get('allow_create'):
         logger.warning("===== unknown entity_id: %s", inputEntity)
@@ -20,18 +22,20 @@ else:
             inputAttributesObject = inputStateObject.attributes.copy()
         else:
             inputAttributesObject = {}
-    
+
         for item in data:
             newAttribute = data.get(item)
-            logger.debug("===== item = {0}; value = {1}".format(item,newAttribute))
+            logger.debug("===== item = {0}; value = {1}".format(item, newAttribute))
             if item == 'entity_id':
-                continue            # already handled
+                continue  # already handled
             elif item == 'allow_create':
-                continue            # already handled
+                continue  # already handled
             elif item == 'state':
                 inputState = newAttribute
+            elif item == 'state_id':
+                inputStateObject = hass.states.get(newAttribute)
+                inputState = inputStateObject.state
             else:
                 inputAttributesObject[item] = newAttribute
-            
-        hass.states.set(inputEntity, inputState, inputAttributesObject)
 
+        hass.states.set(inputEntity, inputState, inputAttributesObject)
